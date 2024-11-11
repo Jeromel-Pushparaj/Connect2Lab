bf4fdcb34838727be3905fa5e9ff3d5d22934f39
Jeromel Pushparaj <jeromal03@gmail.com>
2024-11-11 15:12:59 +0530

    Design of Start Machine Page

--- htdocs/_templates/machine/start.php
+++ htdocs/_templates/machine/start.php
@@ -1,1 +1,14 @@
-<h1>Start</h1>
+<section class="d-flex justify-content-center align-items-center vh-100">
+<div >
+<h1 class="mb-4">Do Something Great With Your Machines!</h1>
+<div class="card shadow" style="width: 18rem;">
+  <div class="card-body">
+    <h5 class="card-title">Machine</h5>
+    <h6 class="card-subtitle mb-2 text-muted">About Machine</h6>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#"  class="btn btn-success my-2">Start</a>
+    <a href="#"  class="btn btn-info my-2">info</a>
+  </div>
+</div>
+</div>
+</section>
